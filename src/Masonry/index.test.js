60832a32688ddbd2328adff65bc3e6e029b06ea6
Cédric Delpoux <cedric.delpoux@gmail.com>
2020-05-12 07:42:02 +0200

    Update test

--- src/Masonry/index.test.js
+++ src/Masonry/index.test.js
@@ -1,9 +1,9 @@
-import { configure, mount } from "enzyme";
-import Adapter from 'enzyme-adapter-react-16';
-import React from "react";
-import Masonry from "./";
+import {configure, mount} from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import React from "react"
+import Masonry from "./"
 
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()})
 
 const content = "Content"
 const MasonryFixture = (
