a7d0d50fce1001fcc422b74b23287df8fc1990ea
Christina Yang <ychristina011@gmail.com>
2024-12-04 10:31:56 -0800

    feat: distribute children by height
    This reverts commit 47cf755711567d75cbedf5cb235ef5173a3aebf9.

--- src/Masonry/index.js
+++ src/Masonry/index.js
@@ -2,22 +2,78 @@ import PropTypes from "prop-types"
 import React from "react"
 
 class Masonry extends React.Component {
-  getColumns() {
+  componentDidUpdate() {
+    if (!this.state.hasDistributed && !this.props.sequential) this.distributeChildren()
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    const {children, columnsCount} = props
+    if (state && children === state.children) return null
+    return {
+      ...Masonry.getEqualCountColumns(children, columnsCount),
+      children,
+      hasDistributed: false,
+    }
+  }
+
+  distributeChildren() {
     const {children, columnsCount} = this.props
+    const columnHeights = Array(columnsCount).fill(0)
+
+    const isReady = this.state.childRefs.every(
+      (ref) => ref.current.getBoundingClientRect().height
+    )
+
+    if (!isReady) return
+
     const columns = Array.from({length: columnsCount}, () => [])
     let validIndex = 0
     React.Children.forEach(children, (child) => {
       if (child && React.isValidElement(child)) {
-        columns[validIndex % columnsCount].push(child)
+        // .current is undefined if ref was passed to a functional component without forwardRef
+        // now passing ref into a wrapper div so it should always be defined
+        const childHeight =
+          this.state.childRefs[validIndex].current.getBoundingClientRect()
+            .height
+        const minHeightColumnIndex = columnHeights.indexOf(
+          Math.min(...columnHeights)
+        )
+        columnHeights[minHeightColumnIndex] += childHeight
+        columns[minHeightColumnIndex].push(child)
         validIndex++
       }
     })
-    return columns
+
+    this.setState((p) => ({...p, columns, hasDistributed: true}))
+  }
+
+  static getEqualCountColumns(children, columnsCount) {
+    const columns = Array.from({length: columnsCount}, () => [])
+    let validIndex = 0
+    const childRefs = []
+    React.Children.forEach(children, (child) => {
+      if (child && React.isValidElement(child)) {
+        const ref = React.createRef()
+        childRefs.push(ref)
+        columns[validIndex % columnsCount].push(
+          <div
+            style={{display: "flex", justifyContent: "stretch"}}
+            key={validIndex}
+            ref={ref}
+          >
+            {child}
+          </div>
+          // React.cloneElement(child, {ref}) // cannot attach refs to functional components without forwardRef
+        )
+        validIndex++
+      }
+    })
+    return {columns, childRefs}
   }
 
   renderColumns() {
     const {gutter, itemTag, itemStyle} = this.props
-    return this.getColumns().map((column, i) =>
+    return this.state.columns.map((column, i) =>
       React.createElement(
         itemTag,
         {
@@ -73,6 +129,7 @@ Masonry.propTypes = {
   containerTag: PropTypes.string,
   itemTag: PropTypes.string,
   itemStyle: PropTypes.object,
+  sequential: PropTypes.bool,
 }
 
 Masonry.defaultProps = {
@@ -83,6 +140,7 @@ Masonry.defaultProps = {
   containerTag: "div",
   itemTag: "div",
   itemStyle: {},
+  sequential: false,
 }
 
 export default Masonry
